e71f659391c4969808e87e0f14a04ae2c17e916b
Sanskar <sanskarjain505@gmail.com>
2023-09-08 01:42:45 +0530

    1.1.1
    -> shared media can be seen in conversation setting section

--- Chat/app/src/main/java/com/stranger/chat/chat_modules/ConversationSetting.java
+++ Chat/app/src/main/java/com/stranger/chat/chat_modules/ConversationSetting.java
@@ -1,14 +1,18 @@
 package com.stranger.chat.chat_modules;
 
 import static com.stranger.chat.fuctionality.Keys.BUNDLE_DATA;
+import static com.stranger.chat.fuctionality.Keys.firebase.MESSAGE_ID;
 import static com.stranger.chat.fuctionality.Keys.firebase.PHONE_NUMBER;
 import static com.stranger.chat.fuctionality.Keys.firebase.PROFILE_PIC_URL;
 import static com.stranger.chat.fuctionality.Keys.firebase.USERNAME;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 import com.stranger.chat.R;
 
@@ -23,13 +30,14 @@ public class ConversationSetting extends AppCompatActivity {
     AppBarLayout appBar;
     CollapsingToolbarLayout collapsingToolbar;
     ImageView profileImageView;
+    TextView username, phoneNumber;
     MaterialToolbar toolBar;
 
-    TextView username, phoneNumber;
+    FloatingActionButton videoCall, audioCall, notification, search, socialMedia;
 
-    LinearLayout videoCall, audioCall, notification, search, socialMedia;
+    LinearLayout sharedMedia, media;
+
     @Override
-
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation_setting);
@@ -39,10 +47,12 @@ public class ConversationSetting extends AppCompatActivity {
         appBar = findViewById(R.id.appBar);
         collapsingToolbar = findViewById(R.id.collapsingToolbar);
         profileImageView = findViewById(R.id.profileImageView);
-        toolBar = findViewById(R.id.toolBar);
-
         username = findViewById(R.id.username);
         phoneNumber = findViewById(R.id.phoneNumber);
+        toolBar = findViewById(R.id.toolBar);
+
+        sharedMedia = findViewById(R.id.sharedMedia);
+        media = findViewById(R.id.media);
 
         toolBar.setNavigationOnClickListener(v -> finish());
 
@@ -59,5 +69,22 @@ public class ConversationSetting extends AppCompatActivity {
         username.setText(bundle.getString(USERNAME));
         phoneNumber.setText(bundle.getString(PHONE_NUMBER));
 
+        StorageReference listRef = FirebaseStorage.getInstance().getReference().child("message/" + bundle.getString(MESSAGE_ID));
+        listRef.listAll().addOnSuccessListener(listResult -> {
+            if (listResult.getItems().size() == 0) {
+                sharedMedia.setVisibility(View.GONE);
+                return;
+            }
+            for (StorageReference item : listResult.getItems()) {
+                // All the items under listRef.
+                View view = LayoutInflater.from(this).inflate(R.layout.sample_image, null);
+                media.addView(view);
+                ImageView img = view.findViewById(R.id.image);
+                item.getDownloadUrl().addOnCompleteListener(task -> Picasso.get().load(task.getResult()).placeholder(R.drawable.image_48px).into(img));
+            }
+        }).addOnFailureListener(e -> {
+            sharedMedia.setVisibility(View.GONE);
+            Toast.makeText(this, "Somthing went wrong in media section", Toast.LENGTH_SHORT).show();
+        });
     }
 }
